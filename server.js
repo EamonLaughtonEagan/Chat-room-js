50c7ecfcc134abfa9e6f7972d7c35b802b82bb3a
unknown <eamonlaughtoneagan@gmail.com>
2019-12-28 21:41:46 -0500

    test

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const http = require('http')
 const app = express()
-const server = require('http').Server(app)
+const server = http.Server(app)
 const io = require('socket.io')(server)
 const PORT = process.env.PORT || 8080
 
